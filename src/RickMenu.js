323b6ade9b28334208329c8f02525a39c37f6045
Daniel Jonathan Zurita Mena <dzurita@cifpfbmoll.eu>
2020-11-23 00:43:24 +0100

    feat(RickMenu): Añadir RickMenu con dispatch()

---
+++ src/RickMenu.js
@@ -0,0 +1,15 @@
+function RickyMenu() {
+
+    this.stock = 100;
+    this.cost = 10;
+
+}
+
+RickyMenu.prototype.dispatch = function (owner) {
+    if (this.stock > 0 && owner.isPayable(this.cost)) {
+        owner.pay(this.cost)
+        this.cost -= 1;
+    }
+}
+
+module.exports = RickyMenu;
