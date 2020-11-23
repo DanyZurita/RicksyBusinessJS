3f63a9c3d9ed65e7e631b5ff115aa09fc72f8354
Daniel Jonathan Zurita Mena <dzurita@cifpfbmoll.eu>
2020-11-23 01:09:48 +0100

    test(CreditCard): Refactorizar casos test añadiendo un beforeEach()

--- test/CreditCard.test.js
+++ test/CreditCard.test.js
@@ -1,14 +1,16 @@
 const CreditCard = require('../src/CreditCard.js');
 
+beforeEach(() => {
+    danyCredit = new CreditCard("Dany", 123);
+});
+
 test('Create CreditCard', () => {
-    let danyCredit = new CreditCard("Dany", 123);
     expect(danyCredit.owner).toBe("Dany");
     expect(danyCredit.number).toBe(123);
     expect(danyCredit.credit).toBe(3000);
 });
 
 test('Create 2 CreditCards', () => {
-    let danyCredit = new CreditCard("Dany", 123);
     let zuritaCredit = new CreditCard("Zurita", 1234);
     expect(danyCredit.owner).toBe("Dany");
     expect(danyCredit.number).toBe(123);
@@ -19,19 +21,16 @@ test('Create 2 CreditCards', () => {
 });
 
 test('Pay 500 credits', () => {
-    let danyCredit = new CreditCard("Dany", 123);
     danyCredit.pay(500);
     expect(danyCredit.credit).toBe(2500);
 });
 
 test('Cant pay 5000 credits', () => {
-    let danyCredit = new CreditCard("Dany", 123);
     danyCredit.pay(5000);
     expect(danyCredit.credit).toBe(3000);
 });
 
 test('Cant pay with 0 credits', () => {
-    let danyCredit = new CreditCard("Dany", 123);
     danyCredit.pay(3000);
     danyCredit.pay(1);
     expect(danyCredit.credit).toBe(0);
