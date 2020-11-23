d35677909c58cc042dc620309f7fdc871c28842e
Daniel Jonathan Zurita Mena <dzurita@cifpfbmoll.eu>
2020-11-23 01:36:18 +0100

    test(UfosPark): Añadir caso test dispatch()

--- test/UfosPark.test.js
+++ test/UfosPark.test.js
@@ -3,6 +3,7 @@ const CreditCard = require('../src/CreditCard.js');
 
 beforeEach(() => {
     danyCredit = new CreditCard("Dany", 123);
+    zuritaCredit = new CreditCard("Zurita", 1234);
     ufosPark = singletonUfosPark.getUfo();
 });
 
@@ -18,5 +19,16 @@ test('Add 3 new Ufo', () => {
         actualFleet.set(ufos[i], null)
         ufosPark.addUfo(ufos[i]);
     }
+    expect(ufosPark.fleet).toMatchObject(actualFleet);
+});
+
+test('Dispatch 2 Ufos', () => {
+    let ufos = ["Ufo1", "Ufo2", "Ufo3"];
+    let actualFleet = new Map().set("Ufo1", danyCredit.number).set("Ufo2", zuritaCredit.number).set("Ufo3", null);
+    for (let i in ufos) {
+        ufosPark.addUfo(ufos[i]);
+    }
+    ufosPark.dispatch(danyCredit);
+    ufosPark.dispatch(zuritaCredit)
     expect(ufosPark.fleet).toMatchObject(actualFleet);
 });
